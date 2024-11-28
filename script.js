70f3aabce1d2ef36d1738cc3552f3b905fcf236e
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-11-28 16:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+	name : 'Jayesh'
+};
+
+object.prototype.getKeys = function() {
+	return object.keys(this);
+};
+
+const keys = student.getKeys();
+console.log(keys);
